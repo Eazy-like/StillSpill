d778a0ca4c366de3ee428533fa26b8102ea0c081
Eazy-like <94602451+Eazy-like@users.noreply.github.com>
2025-02-14 04:50:16 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent page refresh
+    window.location.href = "vent.html"; // Redirect to Venting Space
+});
